5aba527a141fd38ef92c72861d18d598f24a8932
Keith Pitty <keith@keithpitty.com>
2009-06-01 14:17:06 +1000

    Created session table and rake tasks.

--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,5 @@
 # This file is auto-generated from the current state of the database. Instead of editing this file, 
-# please use the migrations feature of ActiveRecord to incrementally modify your database, and
+# please use the migrations feature of Active Record to incrementally modify your database, and
 # then regenerate this schema definition.
 #
 # Note that this schema.rb definition is the authoritative source for your database schema. If you need
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 6) do
+ActiveRecord::Schema.define(:version => 20090601041054) do
 
   create_table "contacts", :force => true do |t|
     t.string "first_name"
@@ -39,6 +39,16 @@ ActiveRecord::Schema.define(:version => 6) do
     t.string "title"
     t.text   "details"
   end
+
+  create_table "sessions", :force => true do |t|
+    t.string   "session_id", :default => "", :null => false
+    t.text     "data"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "sessions", ["session_id"], :name => "index_sessions_on_session_id"
+  add_index "sessions", ["updated_at"], :name => "index_sessions_on_updated_at"
 
   create_table "taggings", :force => true do |t|
     t.integer  "tag_id"
